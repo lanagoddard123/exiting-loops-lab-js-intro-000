dcd77c31b560ae346bdef4f50d4550e9dcd4eb92
lanasgoddard <lanasgoddard@gmail.com>
2019-01-22 13:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ function breakOut(array, changeValue, stopValue){
 }
 function keepGoing(array, changeValue, skipValue){
   for(let i=0;i<array.length;i++){
-    if(array[i]!===skipValue){
+    if(array[i]!=== skipValue){
       array[i]=changeValue
     }
   }
   return array
 }
-    
