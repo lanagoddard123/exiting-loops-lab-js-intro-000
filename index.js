7fc3e6f2e8dacf46a718ef8882cde704ecd67866
lanasgoddard <lanasgoddard@gmail.com>
2019-01-22 13:04:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue){
   for(let i = 0; i< array.length;i++){
    if(array[i]!==stopValue){
-  array.splice([i],changeValue)
+  array[i]=changeValue
 }
  else {
    break
