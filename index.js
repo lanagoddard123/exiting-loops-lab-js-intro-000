5192ccc15ed2e513a30e1b4b5ae27bd9c12cb82c
lanasgoddard <lanasgoddard@gmail.com>
2019-01-22 13:29:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function keepGoing(array, changeValue, skipValue){
 }
 
 Function findBy(array, findFn){
-
+  for (let i = 0; i<array.length; i++) {
+    if(findFn(array[i])){
+      return array[i]
+    }
+  }
 }
