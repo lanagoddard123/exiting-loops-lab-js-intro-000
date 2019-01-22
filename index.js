e5f835d0daa7c5fbc0f7d81d13dad41318adcda5
lanasgoddard <lanasgoddard@gmail.com>
2019-01-22 12:36:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function break0ut(array, changeValue, stopValue){
+  for(let i = 0; i<array.length;i++)
+} if(array[i]!==stopValue){
+  array.splice([i],changeValue)
+} 
+ else {
+   break
+ }
+ return array
+ }
